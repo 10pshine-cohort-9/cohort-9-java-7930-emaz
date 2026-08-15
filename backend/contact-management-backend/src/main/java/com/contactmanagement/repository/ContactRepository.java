9da952bdf1154b16fa18bc450ce3383d13cb7e40
EmazAliKhan <emaz.kiet26@gmail.com>
2026-08-15 14:27:41 +0500

    fix: add email and phone in search

--- backend/contact-management-backend/src/main/java/com/contactmanagement/repository/ContactRepository.java
+++ backend/contact-management-backend/src/main/java/com/contactmanagement/repository/ContactRepository.java
@@ -20,10 +20,15 @@ public interface ContactRepository extends JpaRepository<Contact, Long> {
     // For list results
     List<Contact> findByUserId(Long userId);
 
-    @Query("SELECT c FROM Contact c WHERE c.user.id = :userId AND " +
+    @Query("SELECT DISTINCT c FROM Contact c " +
+            "LEFT JOIN c.emails e " +
+            "LEFT JOIN c.phones p " +
+            "WHERE c.user.id = :userId AND " +
             "(LOWER(c.firstName) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
             "LOWER(c.lastName) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
-            "LOWER(c.title) LIKE LOWER(CONCAT('%', :search, '%')))")
+            "LOWER(c.title) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
+            "LOWER(e.value) LIKE LOWER(CONCAT('%', :search, '%')) OR " +
+            "LOWER(p.value) LIKE LOWER(CONCAT('%', :search, '%')))")
     Page<Contact> searchContacts(@Param("userId") Long userId,
                                  @Param("search") String search,
                                  Pageable pageable);
